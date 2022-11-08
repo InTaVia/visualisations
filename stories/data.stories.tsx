2810792e8b9e558c002c41f012072785e4bc497c
Johannes Liem <joh.liem@gmail.com>
2022-11-08 11:17:26 +0100

    control for fixtures (1,2,20,100)

--- stories/data.stories.tsx
+++ stories/data.stories.tsx
@@ -1,4 +1,14 @@
+import entities001 from "./fixtures/entities-1.json";
+import entities002 from "./fixtures/entities-2.json";
+import entities020 from "./fixtures/entities-20.json";
 import entities100 from "./fixtures/entities-100.json";
+
+const entityGroups = {
+  1: entities001,
+  2: entities002,
+  20: entities020,
+  100: entities100
+}
 
 interface DataViewProps {
   entities: Record<string, Record<string, any>>
@@ -21,10 +31,14 @@ function DataView(props: DataViewProps): JSX.Element {
 }
 
 // TODO: make control to select different fixtures
-const meta = {};
+const meta = {
+  argTypes: {
+    entitiesCount: { control: { type: "select", options: Object.keys(entityGroups)}, defaultValue: Object.keys(entityGroups).at(-1),},
+  },
+};
 
 export default meta;
 
-export const Test = () => {
-  return <DataView entities={entities100} />;
+export const Data = (args: any) => {
+  return <DataView entities={entityGroups[args.entitiesCount as number] as DataViewProps["entities"]} />;
 };
