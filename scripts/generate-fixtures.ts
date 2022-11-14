8f8047618f1c5aeafe73013c061b50851029379e
Johannes Liem <joh.liem@gmail.com>
2022-11-14 11:55:16 +0100

    chore: generate fixtures extracts events from entities (#3)
    * chore: generate fixtures extracts events from entities
    
    * using intavia data model in generate fixtures

--- scripts/generate-fixtures.ts
+++ scripts/generate-fixtures.ts
@@ -7,6 +7,8 @@ import { log } from "@stefanprobst/log";
 import { format } from "prettier";
 import { keyBy } from "@stefanprobst/key-by";
 
+import type { Entity, EntityEvent } from "@/api/intavia.models"
+
 const baseUrl = "https://intavia-backend.acdh-dev.oeaw.ac.at";
 
 const dataset = {
@@ -28,22 +30,35 @@ async function generate(limit: number) {
   });
 
   const data = await request(url, options);
-  const entitiesById = keyBy(data.results, (entity) => entity.id);
+  
+  //reduce events array to array of event ids
+  const entities: Array<Entity> = data.results.map((entity: Entity) => {return {...entity, events: entity.events!.map((event) => event.id as EntityEvent['id'])}});
+  const entitiesById = keyBy(entities, (entity) => entity.id);
+
+  //extract events from entities; Set used to remove duplicates comming from different entitites
+  const events: Array<EntityEvent> = Array.from(new Set(data.results.flatMap((entity: Entity) => entity.events)));
+  const eventsById = keyBy(events, (event) => event['id']);
 
   const fixturesFolder = join(process.cwd(), "stories", "fixtures");
-  const filePath = join(fixturesFolder, `entities-${limit}.json`);
+  const filePathEntities = join(fixturesFolder, `entities-${limit}.json`);
   await writeFile(
-    filePath,
+    filePathEntities,
     format(JSON.stringify(entitiesById), { parser: "json" }),
+    { encoding: "utf-8" }
+  );
+  const filePathEvents = join(fixturesFolder, `events-${limit}.json`);
+  await writeFile(
+    filePathEvents,
+    format(JSON.stringify(eventsById), { parser: "json" }),
     { encoding: "utf-8" }
   );
 }
 
 [1, 2, 20, 100].forEach(limit =>{ generate(limit)
   .then(() => {
-    log.success("Successfully generated fixtures.");
+    log.success(`Successfully generated entity and event fixtures for entity count = ${limit}`);
   })
   .catch((error) => {
-    log.error("Failed to generate fixtures.\n", String(error));
+    log.error(`Failed to generate entity and event fixtures for ntity count = ${limit} .\n`, String(error));
   });
 })
