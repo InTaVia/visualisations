a4f9db69f5e3ad53f51252960d7095ea21fde4f1
Johannes Liem <joh.liem@gmail.com>
2022-11-25 12:06:41 +0100

    fix: lat lon swapping fix-gen for place entites

--- scripts/generate-fixtures.ts
+++ scripts/generate-fixtures.ts
@@ -33,25 +33,40 @@ async function generate(limit: number) {
     [entity],
   )
 
-  const entitiesById = entities.entities
+  const _entitiesById = entities.entities
   const _eventsById = entities.events
 
-  const _events = Object.values(_eventsById)
+  const _entities = Object.values(_entitiesById)
     // FIXME: the backend currently returns incorrect geojson coordinates in [lat, lng], but geojson requires [lng, lat].
-    .map((event) => {
-      if (event.place?.geometry == null || event.place.geometry.type !== 'Point') return event
+    .map((entity) => {
+      if (entity.kind !== 'place') return entity
+      if (entity.geometry == null || entity.geometry.type !== 'Point') return entity
 
       return {
-        ...event,
-        place: {
-          ...event.place,
-          geometry: {
-            ...event.place.geometry,
-            coordinates: [event.place.geometry.coordinates[1], event.place.geometry.coordinates[0]],
-          },
+        ...entity,
+        geometry: {
+          ...entity.geometry,
+          coordinates: [entity.geometry.coordinates[1], entity.geometry.coordinates[0]],
         },
       }
     })
+
+  const _events = Object.values(_eventsById)
+    // FIXME: the backend currently returns incorrect geojson coordinates in [lat, lng], but geojson requires [lng, lat].
+    // .map((event) => {
+    //   if (event.place?.geometry == null || event.place.geometry.type !== 'Point') return event
+
+    //   return {
+    //     ...event,
+    //     place: {
+    //       ...event.place,
+    //       geometry: {
+    //         ...event.place.geometry,
+    //         coordinates: [event.place.geometry.coordinates[1], event.place.geometry.coordinates[0]],
+    //       },
+    //     },
+    //   }
+    // })
     // FIXME: the backend currently does not return event kinds.
     .map((event) => {
       const id = event.id.slice(0, event.id.lastIndexOf('/'))
@@ -67,6 +82,10 @@ async function generate(limit: number) {
     })
   const eventsById = keyBy(_events, (event) => {
     return event.id
+  })
+
+  const entitiesById = keyBy(_entities, (entity) => {
+    return entity.id
   })
 
   const fixturesFolder = join(process.cwd(), 'stories', 'fixtures')
