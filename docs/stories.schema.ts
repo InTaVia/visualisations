eb5521a65963e3ddef4a5b28e8fa89e47d9b1082
Johannes Liem <joh.liem@gmail.com>
2023-01-22 15:29:38 +0100

    update story-script-schema

--- docs/stories.schema.ts
+++ docs/stories.schema.ts
@@ -17,7 +17,8 @@ export interface Slide {
   id: string
   title: string
   sort: number
-  visualisationsPanes: Record<string, Visualisation>
+  layout: 'con-con' | 'con' | 'title' | 'vis-con' | 'vis'
+  visualisationPanes: Record<string, Visualisation>
   contentPanes: Record<string, ContentPane>
 }
 
@@ -29,20 +30,49 @@ export const defaultStyles = {
 
 export interface ContentPane {
   id: string
-  contents: Array<Content>
+  contents: Array<ContentBase>
 }
 
-export interface Content {
+export interface ContentBase {
   id: string
   title: string
   sort: number
   type: '3DObject' | 'Audio' | 'Headline' | 'Image' | 'IndexSlide' | 'Quiz' | 'Text' | 'Video'
 }
 
+export interface Headline extends ContentBase {
+  type: 'Headline'
+  format: 'H1' | 'H2' | 'H3'
+  alignment: 'center' | 'left' | 'right'
+  text: string
+}
+
+export interface Text extends ContentBase {
+  type: 'Text'
+  text: string
+}
+
+export interface Image extends ContentBase {
+  type: 'Text'
+  url: string
+  copyright: string
+  caption: string
+}
+
+export interface Quiz extends ContentBase {
+  type: 'Quiz'
+  question: string
+  answers: Array<QuizAnswer>
+}
+
+export interface QuizAnswer {
+  html: string
+  correct: boolean
+}
+
 export interface Visualisation {
   id: string
   type: 'Map' | 'Network' | 'Set' | 'Statistic' | 'Timeline' // rather meta types or the concrete types?
-  // data: Array<Record<EntityEvent['id'] | Entity['id'], Entity | EntityEvent>>;
   highlighted: boolean
   entities: Array<unknown>
   events: Array<unknown>
@@ -64,10 +94,6 @@ export interface MarkerProperties {
   coords: [number, number]
 }
 
-/* defaultMarkerProperties {
-  "DeathEvent": {hidden: false, fill: "purple"},
-} */
-
 export interface MarkerClusterMap extends Map {
   subtype: 'MarkerClusterMap'
   clusterThemeAttribute: string | null // e.g. 'entity-kind'  | 'event-kind' | null for NO-clustering at all
@@ -75,141 +101,6 @@ export interface MarkerClusterMap extends Map {
   highlighed?: Array<EntityEvent['id']>
   events: Array<EntityEvent>
 }
-
-// Data Requirements
-// Filter events: event-kind in ['birthevent', 'deathevent']
-// eventTypes.some(e => ['birthevent', 'deathevent'].includes(e))
-// id: 'ArmasMarkerClusterMap',
-// type: 'Map',
-// subtype: 'MarkerClusterMap',
-// clusterType: 'donut',
-// clusterThemeAttribute: 'event-kind',
-// highlighed: ["http://www.intavia.eu/bs/birthevent/1000"],
-// events: [
-//   {
-//       "id": "http://www.intavia.eu/bs/birthevent/1000",
-//       "label": {
-//         "default": "Birth of Taipale, Armas"
-//       },
-//       "startDate": "1890-07-27T00:00:00.000Z",
-//       "endDate": "1890-07-27T23:59:59.000Z",
-//       "place": {
-//         "id": "http://www.intavia.eu/bs/place/1757",
-//         "label": {
-//           "default": "Hlsinki"
-//         },
-//         "geometry": {
-//           "coordinates": [
-//             60.1659,
-//             24.989
-//           ],
-//           "type": "Point"
-//         },
-//         "kind": "place"
-//       },
-//       "relations": [
-//         {
-//           "id": "http://www.intavia.eu/bs/personproxy/1000",
-//           "label": {
-//             "default": "Taipale, Armas"
-//           },
-//           "entity": {
-//             "id": "http://www.intavia.eu/bs/personproxy/1000",
-//             "label": {
-//               "default": "Taipale, Armas"
-//             },
-//             "linkedIds": [
-//               {
-//                 "id": "p1000",
-//                 "provider": {
-//                   "label": "BiographySampo",
-//                   "baseUrl": "http://ldf.fi/nbf/"
-//                 }
-//               },
-//               {
-//                 "id": "Q679220",
-//                 "provider": {
-//                   "label": "Wikidata",
-//                   "baseUrl": "http://www.wikidata.org/entity"
-//                 }
-//               }
-//             ],
-//             "gender": {
-//               "id": "http://ldf.fi/schema/bioc/Male",
-//               "label": {
-//                 "default": "Male"
-//               }
-//             },
-//             "kind": "person"
-//           },
-//           "role": {
-//             "id": "http://www.intavia.eu/bs/born_person/1000"
-//           }
-//         }
-//       ]
-//     },
-//    {
-//       "id": "http://www.intavia.eu/bs/deathevent/1000",
-//       "label": {
-//         "default": "Death of Taipale, Armas"
-//       },
-//       "startDate": "1976-01-01T00:00:00.000Z",
-//       "endDate": "1976-12-31T23:59:59.000Z",
-//       "place": {
-//         "id": "http://www.intavia.eu/bs/place/38511",
-//         "label": {
-//           "default": "Turku"
-//         },
-//         "geometry": {
-//           "coordinates": [
-//             60.4504,
-//             22.2479
-//           ],
-//           "type": "Point"
-//         },
-//         "kind": "place"
-//       },
-//       "relations": [
-//         {
-//           "id": "http://www.intavia.eu/bs/personproxy/1000",
-//           "label": {
-//             "default": "Taipale, Armas"
-//           },
-//           "entity": {
-//             "id": "http://www.intavia.eu/bs/personproxy/1000",
-//             "label": {
-//               "default": "Taipale, Armas"
-//             },
-//             "linkedIds": [
-//               {
-//                 "id": "p1000",
-//                 "provider": {
-//                   "label": "BiographySampo",
-//                   "baseUrl": "http://ldf.fi/nbf/"
-//                 }
-//               },
-//               {
-//                 "id": "Q679220",
-//                 "provider": {
-//                   "label": "Wikidata",
-//                   "baseUrl": "http://www.wikidata.org/entity"
-//                 }
-//               }
-//             ],
-//             "gender": {
-//               "id": "http://ldf.fi/schema/bioc/Male",
-//               "label": {
-//                 "default": "Male"
-//               }
-//             },
-//             "kind": "person"
-//           },
-//           "role": {
-//             "id": "http://www.intavia.eu/bs/deceased_person/1000"
-//           }
-//         }
-//       ]
-//     }
 
 export interface TrajectoryMap extends Map {
   id: string
